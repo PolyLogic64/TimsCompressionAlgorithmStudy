a8320d06bab540a00e4984db0adbc08568fe70d9
PolyLogic64 <50244580+PolyLogic64@users.noreply.github.com>
2020-06-26 14:29:01 +0200

    Add project files.

---
+++ CompressionAlgorithms/HuffmanCoding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class HuffmanCoding
+{
+    public class HuffmanNode<T>
+    {
+        public HuffmanNode<T> child1;
+        public HuffmanNode<T> child2;
+        public int frequency;
+        public T symbol;
+    }
+    public void Shit(string s)
+    {
+
+    }
+    public HuffmanCoding()
+	{
+		
+	
+	}
+}
---
+++ CompressionAlgorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace CompressionAlgorithms
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+
+            
+
+            
+        }
+    }
+}
